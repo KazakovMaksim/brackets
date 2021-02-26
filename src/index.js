928de3b5eb55a98ce16b6ef91000b3a9936cf505
KazakovMaksim <kazakovmaksimi@gmail.com>
2021-02-26 16:52:32 +0300

    add script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  
+  let count = 0;
+  let arrOpen = [];
+  let arrClose = [];
+  console.log(str);
+  let result;           
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        arrOpen.push(bracketsConfig[i][0]);
+        arrClose.push(bracketsConfig[i][1]);
+    }
+
+  
+    
+      for (let j = 0; j < bracketsConfig.length;  j++) {
+        
+       
+        count = 0;
+          
+         for (let i = 0; i < str.length; i++) {
+      
+            if (str[i] === arrOpen[j]) {
+               count++;
+              
+            } else if (str[i] === arrClose[j]) {
+               count--;
+              
+            }
+         
+          if (count < 0 ) {
+            result = false;
+            console.log(result);
+             return result;
+               break; 
+          } else if (count % 2 > 0 && i == str.length-1){
+            result = false;
+            return result;
+        }
+          }  
+          
+         }
+         result = true;
+            console.log(result);
+         return result;
+        }
